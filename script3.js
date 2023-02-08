e7c5f07197120079f81e5d6fddcb96f02c1db682
ArokiaSamy45 <dossarokia45@gmail.com>
2023-02-08 16:41:24 +0530

    second commit

---
+++ script3.js
@@ -0,0 +1,67 @@
+// let a1;
+// let a2;
+// let a3;
+// let a4;
+// console.log(a1, a2, a3, a4);
+// ------------------------------------------
+// var myvar= 1;
+// console.log(myvar);
+// ---------------------------------------
+// let firstName = "Arokia";
+// let lastName = "Samy";
+// let maritalStatus = "single";
+// let country = "India";
+// let age = 26;
+// console.log(firstName, lastName, maritalStatus, country, age);
+// ------------------------------------------------
+// let firstName = "Arokia", lastName = "Samy", maritalStatus = "single", country = "India", age = 26;
+// console.log(firstName, lastName, maritalStatus, country, age);
+// ----------------------------------------------------
+// let string = "Hello";
+// let boolean = true;
+// let undef = undefined;
+// let nul = null;
+// console.log(string, boolean, undef, nul);
+// --------------------------------------------------
+// let string = "25";
+// let int1 = parseInt(string);
+// let int2 = Number(string);
+// let int3 = +string;
+// console.log(int1, int2, int3);
+// ----------------------------------------
+// console.log(Boolean(1));
+// console.log(Boolean("Hello"));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// ----------------------------------
+// let num = 4;
+// let square = num * num;
+// console.log("Square of " + num + " is " + square);
+// ----------------------------------
+// let a = 10;
+// let b = 20;
+// [a, b] = [b, a];
+// console.log("a: " + a + " b: " + b);
+// ---------------------------------
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let sum = a + b + c;
+// console.log("Sum of " + a + ", " + b + " and " + c + " is " + sum);
+// ----------------------------------------------------------
+// let celsius = 100;
+// let fahrenheit = (celsius * 9/5) + 32;
+// console.log(celsius + "°C is equal to " + fahrenheit + "°F");
+// ---------------------------------------------
+// let meter = 1000;
+// let miles = meter / 1609.34;
+// console.log(meter + " meters is equal to " + miles + " miles");
+
+
+
+
+
+
+
